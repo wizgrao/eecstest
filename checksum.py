f86b5781785fbb8e1027a8368abfe642a54333dd
Vishnu Doppalapudi <vishnu234@berkeley.edu>
2018-11-09 17:19:55 -0800

    Incorporated metadata into checksum

--- checksum.py
+++ checksum.py
@@ -1,13 +1,26 @@
 import numpy as np
 
 class Packet:
-    def __init__(self, chunks):
+    def __init__(self, input):
+        '''
+        input = String input of bits, length 128 + 32 representing xor of packets
+            - Last 32 bits corresponds to the metadata
+
+        '''
         self.chunk_size = 16
-        self.checksum = self.get_checksum(list(chunks.values()))
-        self.data = list(chunks.values())
-        #self.meta_data = self.get_metadata(list(chunks.keys()))
+        self.data = [input[16*i:16*(i+1)] for i in range(128//16)] #128 Bits
+        self.meta_data = input[128:] #32 Bits
+        self.total_data = list(self.data)
+        total_data.append(meta_data[:16])
+        total_data.append(meta_data[16:])
+        self.checksum = self.get_checksum() #16 bits
+
 
     def get_complement_sum(self, one, two):
+        '''
+        Determines complement sum of two equal-sized binary numbers
+        Helper function for get_checksum
+        '''
         out = [0 for _ in range(self.chunk_size)]
         carry = 0
         one = [int(i) for i in one]
@@ -41,15 +54,23 @@ class Packet:
         print(out)
         return np.array(out)
 
-    def get_checksum(self, chunks):
+    def get_checksum(self):
+        '''
+        Input: Chunks xor'd together, broken into 16-bit length strings
+        Output: Internet checksum of these chunks
+        '''
         curr_checksum = np.zeros(self.chunk_size)
+        chunks = self.total_data
+        # for i in range(len(self.data)//self.chunk_size):
+        #     chunks.append(self.data[self.chunk_size*i:self.chunk_size*(i+1)])
+        # Converts string to bit array
         for i in range(len(chunks)):
             new_chunk = ''
             chunk = chunks[i]
             for j in range(len(chunk)):
                 new_chunk += (chunk[j] + ' ')
             chunks[i] = new_chunk
-        chunks = [np.array(chunk.split()) for chunk in chunks]
+        chunks = [np.array([chunk.split()]) for chunk in chunks]
         for chunk in chunks:
             curr_checksum = self.get_complement_sum(curr_checksum, chunk)
         for i in range(len(curr_checksum)):
