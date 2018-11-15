bb490d5e1182225533c7a85aec87410c40e8b7c6
wizgrao <gauravity@berkeley.edu>
2018-11-14 17:37:34 -0800

    fix receive py

--- receive.py
+++ receive.py
@@ -4,19 +4,27 @@ from HuffmanCode import *
 from checksum import *
 from sound import transmit, receive
 from bitarray import bitarray
+from checksum import Packet as Pack
 a = HuffmanCode()
 
 
 c = Receiver()
 
-packets = receive(packet_size=128+16+32, baud=300, signal_cf=800, clock_cf=1400, fdev=300, fs=11025)
+packets = receive(packet_size=128+16+32, baud=300, signal_cf=800, clock_cf=1400, fdev=300, fs=11025, duration=10)
 
 count=0
-while not c.isDone() and count<len(packet):
-    received=packet[count]
-    check=Packet(received, sent=True)
+while not c.isDone() and count<len(packets):
+    received=packets[count]
+    s = ""
+    for b in received:
+        s+=str(b)
+    if len(s) != 128+16+32:
+        count+=1
+        continue
+    check=Pack(s, sent=True)
     if check.check_checksum():
-        tem=check.get_received_packet()
+        print("received packet")
+        temp=check.get_received_packet()
         temp=bytearray(temp, 'utf8')
         c.receive_packet(temp)
     count+=1
