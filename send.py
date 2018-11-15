ff6eadf430284b52ab51f693d9b8ce05beb41a3a
wizgrao <gauravity@berkeley.edu>
2018-11-14 16:59:58 -0800

    receive.py

--- send.py
+++ send.py
@@ -6,17 +6,23 @@ from bitarray import bitarray
 a = HuffmanCode()
 encode=a.compress("test.txt")
 
-#create packet
 b = Transmitter(encode)
 chunks=b.chunks
 packet = b.encode()
-
-bits = ""
+final_packet=[]
 for p in packet:
+    check=Packet(p)
+    # print(check.checksum)
+    # print(check.total_data)
+
+    final_packet.append(check.get_final_packet())
+
+#
+bits = ""
+for p in final_packet:
     bits += p.decode('utf8')
 
 
 transmit(bitarray(bits), baud=200, signal_cf=800, clock_cf=1400, fdev=300, fs=11025, packet_size=128+16+32)
 
 sd.wait()
-
